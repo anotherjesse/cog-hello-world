eaf7dc01d506b772d5cab26a599cdc84d011a5de
Jesse Andrews <anotherjesse@gmail.com>
2023-10-26 11:50:44 -0400

    update the inputs

--- predict.py
+++ predict.py
@@ -3,5 +3,5 @@ from typing import List
 
 
 class Predictor(BasePredictor):
-    def predict(self, count: int = Input(description="How yolo are you - how many images of yolo do you need???", default=1)) -> List[Path]:
+    def predict(self, count: int = Input(description="How yolo are you? how many images of yolo do you need???", default=1)) -> List[Path]:
         return [Path("yolo.png") for i in range(count)]
